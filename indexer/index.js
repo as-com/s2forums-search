2af39e5677cde52f590ef6d68fba0a47ad7c1d59
Andrew Sun <me@andrewsun.com>
2016-01-09 23:41:29 -0500

    there seem to be some caching issues

--- indexer/index.js
+++ indexer/index.js
@@ -3,6 +3,7 @@ var request = require("request").defaults({
 	headers: {
 		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36 comp09/s2forumsbot",
 		"Host": "scratch.mit.edu",
+		"Accept-Language": "en-US,en;q=0.8"
 	},
 	strictSSL: false
 });
